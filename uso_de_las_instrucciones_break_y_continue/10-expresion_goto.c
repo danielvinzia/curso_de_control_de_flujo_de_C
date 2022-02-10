15284df9811a73974b9b0fae4deb18979473178f
danielvinzia <danielvinzia@gmail.com>
2022-02-10 12:30:34 -0300

    clase de la instruccion goto concluida

---
+++ uso_de_las_instrucciones_break_y_continue/10-expresion_goto.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*EJEMPLO DE GOTO, LO QUE HACE ES ENVIARTE A OTRO LUGAR DEL CODIGO HACIENDO QUE SE TERMINE
+LA FORMA SECUENCIAL DEL MISMO */
+
+/*
+int main()
+{
+    for (size_t i = 0; i < count; i++)
+    {
+        for (size_t i = 0; i < count; i++)
+        {
+            for (size_t i = 0; i < count; i++)
+            {
+                if (panic)
+                goto errorHandler;
+            }
+            
+        }
+        
+    }
+    
+    errorHandler: 
+    //todo el codigo para componer tu desastre
+    
+    return 0;
+}
+*/
