0d06578a9d8db5f5d9e7692e275b6e70ab7c2144
danielvinzia <danielvinzia@gmail.com>
2022-02-10 10:30:03 -0300

    clase de introduccion a la toma de decisiones completada

---
+++ toma_de_deciciones/01-estructura_de_decision.c
@@ -0,0 +1,25 @@
+// EJEMPLO DE COMO ESCRIBIR UN IF
+// SE PUEDEN PONER TODOS LOS ELSE IF QUE QUIERA
+/*
+if (expresion) {
+    statement1
+    statement1.2
+    statement1.3
+}
+else if (expresion)
+{
+    statement2
+}
+else if (expresion)
+{
+    statement3
+}
+else if (expresion)
+{
+    statement4
+}
+else
+{
+    statement5
+}
+*/
